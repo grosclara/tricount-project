d056148ec1c1d4e61034fd7be46829d290acac60
Clara Gros <gros.clara91@gmail.com>
2022-03-29 09:49:23 +0200

    Write test list for ExpenseRecorder business class

--- src/errors/InvalidAmountError.ts
+++
@@ -1,7 +0,0 @@
-export default class InvalidAmountError extends Error {
-    constructor(msg: string) {
-        super(msg);
-        // Set the prototype explicitly.
-        Object.setPrototypeOf(this, InvalidAmountError.prototype);
-    }
-}
--- test/expenseRecorder.test.ts
+++ test/expenseRecorder.test.ts
@@ -8,17 +8,6 @@ const expect = chai.expect;
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
-import Expense from '../src/hexagon/models/Expense';
-import User from '../src/hexagon/models/User';
-import { ExpenseRecorder } from '../src/hexagon/usecases/ExpenseRecorder';
-import InvalidAmountError from '../src/errors/InvalidAmountError';
-
-// test avec montant négatif [test avec montant qui n'est pas un chiffre]
-// test avec username qui n'existe pas
-// test qui fonctionne (amount >0 / user valide)
-
-//RecordExpense( amount: number, username: string ) : Expense ;
-
 describe('ExpenseRecorder', () => {
 	it.skip('GIVEN a new expense recorder, WHEN adding an expense with a negative amount, THEN it should fail with an invalid amount error');
 	it.skip('GIVEN a new expense recorder, WHEN adding an expense with an unknown user, THEN it should fail with unknown user error');
